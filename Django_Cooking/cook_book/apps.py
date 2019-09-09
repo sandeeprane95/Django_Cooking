cfb18ece081bef636af479ff847a0d8809cf7df3
Sandeep Rane <srane@codal.com>
2019-09-08 19:06:37 -0500

    Initial Setup

---
+++ Django_Cooking/cook_book/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CookBookConfig(AppConfig):
+    name = 'cook_book'
---
+++ Django_Cooking/cook_book/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Step(models.Model):
+	pass
+
+class Ingredient(models.Model):
+	pass
+
+class Recipe(models.Model):
+	pass
